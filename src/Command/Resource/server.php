66274d7f75a6f1bbdc968bd4775328013bf92598
lanhai <363387485@qq.com>
2024-07-20 21:25:57 +0800

    auto commit at 2024-07-20 21:25:57

--- src/Command/Resource/server.php
+++ src/Command/Resource/server.php
@@ -13,4 +13,8 @@ return [
     'max_mysql_execute_limit' => 1280,//每秒最大mysql处理量限制,超过此值将拒绝服务
     'package_max_length' => 10 * 1024 * 1024,//最大请求数据限制,默认:10M
     'dashboard_password' => null,//控制台超级密码
+    'redis_queue_in_master' => true,
+    'redis_queue_in_slave' => false,
+    'crontab_in_master' => true,
+    'crontab_in_slave' => false
 ];
