58b08f2b4ddf9f8f3424f34b0e2a4d814bc81048
lanhai <363387485@qq.com>
2024-07-21 17:24:29 +0800

    auto commit at 2024-07-21 17:24:29

--- src/Server/Task/Crontab.php
+++ src/Server/Task/Crontab.php
@@ -419,12 +419,14 @@ class Crontab {
             return;
         }
         $this->processingStart();
+        Console::info("【Crontab】任务开始执行:" . $id);
         try {
             $this->run();
         } catch (Throwable $throwable) {
             Log::instance()->error("【Crontab】任务执行失败:" . $throwable->getMessage());
             $this->log("任务执行失败:" . $throwable->getMessage());
         }
+        Console::info("【Crontab】任务开始完成:" . $id);
         $this->processingFinish(time() + $timeout);
         $timerId = Timer::after($timeout * 1000, function () use ($timeout, $id) {
             $this->loop($timeout, $id);
