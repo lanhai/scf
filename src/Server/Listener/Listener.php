f26c1c59028f0552a21b9387d64ff633edae49a8
lanhai <363387485@qq.com>
2024-08-02 10:02:51 +0800

    auto commit at 2024-08-02 10:02:51

--- src/Server/Listener/Listener.php
+++ src/Server/Listener/Listener.php
@@ -4,9 +4,10 @@ namespace Scf\Server\Listener;
 
 use Scf\Server\Http;
 use Swoole\WebSocket\Server;
+use Swoole\Http\Server as HttpServer;
 
 class Listener {
-    protected Server $server;
+    protected Server|HttpServer $server;
 
     public function __construct($server) {
         $this->server = $server;
