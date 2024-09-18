6a423d2c67ff43803649f547754b7bc35be88d60
lanhai <363387485@qq.com>
2024-09-18 08:02:35 +0800

    auto commit at 2024-09-18 08:02:35

--- src/Server/Listener/WorkerListener.php
+++ src/Server/Listener/WorkerListener.php
@@ -56,7 +56,7 @@ INFO;
             if ($enableStatistics && App::isMaster()) {
                 StatisticModel::instance()->updateDB();
             }
-            Http::instance()->enable();
+            Runtime::instance()->set('_SERVER_STATUS_', STATUS_ON);
             Runtime::instance()->set('SERVER_START_STATUS', true);
             //启动任务管理器
 //            $reloadTimes = Counter::instance()->get('_HTTP_SERVER_RESTART_COUNT_') ?? 0;
