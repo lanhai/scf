e15d59152f32b1a02050b63810ea29c855892a0b
363387485@qq.com <363387485@qq.com>
2025-02-28 15:47:31 +0800

    auto commit at 2025-02-28 15:47:31

--- src/Core/Server.php
+++ src/Core/Server.php
@@ -38,20 +38,32 @@ abstract class Server {
      * @return int
      */
     public static function getUseablePort($port): int {
+        if (self::isPortInUse($port)) {
+            return self::getUseablePort($port + 1);
+        }
+        return $port;
+    }
+
+    /**
+     * 检查端口号是否被占用
+     * @param int $port
+     * @return bool
+     */
+    public static function isPortInUse(int $port): bool {
         try {
             $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
             if ($socket === false) {
-                return $port;
+                return false;
             }
             $result = @socket_bind($socket, '0.0.0.0', $port);
             socket_close($socket);
             if ($result === false) {
-                return self::getUseablePort($port + 1);
+                return true;
             }
+            return false;
         } catch (Throwable) {
-            return self::getUseablePort($port + 1);
+            return true;
         }
-        return $port;
     }
 
 }
