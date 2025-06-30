dc566ae0c43efa87884d2b8096e99695a045acfe
363387485@qq.com <363387485@qq.com>
2025-06-30 17:32:15 +0800

    auto commit at 2025-06-30 17:32:15

--- src/version.php
+++ src/version.php
@@ -1,7 +1,7 @@
 <?php
   return array (
   'build' => 'development',
-  'version' => '1.0.67',
-  'url' => 'https://lky-chengdu.oss-cn-chengdu.aliyuncs.com/scf/1.0.67.update',
+  'version' => '1.0.68',
+  'url' => 'https://lky-chengdu.oss-cn-chengdu.aliyuncs.com/scf/1.0.68.update',
   'boot' => 'https://lky-chengdu.oss-cn-chengdu.aliyuncs.com/scf/boot',
 );
